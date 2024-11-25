ebdae71b5f5f9dfe807b65aff12a0928b60df8d1
fadelkaadan <fadelkaadan@gmail.com>
2024-11-25 23:47:38 +0100

    Fix

--- src/components/comments/commentButton/index.tsx
+++ src/components/comments/commentButton/index.tsx
@@ -1,12 +1,13 @@
 import { ChevronUpIcon } from "@heroicons/react/16/solid";
 import { ChatBubbleOvalLeftIcon } from "@heroicons/react/24/outline";
+import { ReactElement } from "react";
 
 interface CommentsProps {
   comments: number[];
   isOpen?: boolean;
   onClick: () => void;
   label: string;
-  icon?: any;
+  // icon?: ReactElement;
 }
 
 const CommentsButton: React.FC<CommentsProps> = ({
@@ -14,7 +15,6 @@ const CommentsButton: React.FC<CommentsProps> = ({
   isOpen,
   onClick,
   label,
-  icon,
 }) => {
   const getCommentsCount = () => (comments ? comments.length : 0);
   const isDisabled = getCommentsCount() === 0;
@@ -25,7 +25,8 @@ const CommentsButton: React.FC<CommentsProps> = ({
       className={`${
         isDisabled
           ? "text-textDisabled border-textDisabled"
-          : "text-gray-300 border-gray-300 hover:text-white hover:border-white"}
+          : "text-gray-300 border-gray-300 hover:text-white hover:border-white"
+      }
         flex bg-transparent items-center justify-center gap-1 border border-textPrimary
         text-textPrimary rounded-md py-1 pl-1.5 pr-2 outline-none text-nowrap w-auto text-xs mt-2`}
     >
