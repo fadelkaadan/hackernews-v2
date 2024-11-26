1a4c22273be8404d0922accb0c5c93bf9c442d45
fadelkaadan <fadelkaadan@gmail.com>
2024-11-26 22:14:45 +0100

    Filter stories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { fetchStories } from "../lib/api";
 // TODO:
 // - Add comment section
 // - Add load more functionality
+// - Add more categories
 
 export default async function Page() {
   const stories = await fetchStories();
@@ -11,9 +12,9 @@ export default async function Page() {
   return (
     <div className="flex justify-center">
       <div className="flex flex-col items-center gap-7 w-11/12 max-w-4xl">
-        {stories.map((story) => (
-          <Card key={story.id} data={story} />
-        ))}
+        {stories.map((story) => {
+          if (story.url) return <Card key={story.id} data={story} />;
+        })}
       </div>
     </div>
   );
