e7898342d24d161b6f5ba83662eea73372129d4d
Fadel Kaadan <fadelkaadan@gmail.com>
2024-11-27 18:20:00 +0100

    Increase stories in feed

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,7 +4,7 @@ const BASE_URL = "https://hacker-news.firebaseio.com/v0/";
 const TOP_STORIES_ENDPOINT = BASE_URL + "topstories.json";
 const ITEM_ENDPOINT = BASE_URL + "item";
 
-export const fetchStories = async (limit: number = 10): Promise<IStory[]> => {
+export const fetchStories = async (limit: number = 20): Promise<IStory[]> => {
   const idsResponse = await fetch(TOP_STORIES_ENDPOINT, { cache: "no-store" });
   const storyIds: number[] = await idsResponse.json();
 
